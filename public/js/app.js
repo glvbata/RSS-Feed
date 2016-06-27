8f32c1be965c1ed810bdb2b07eccea05c32c96f2
Gian Bata <glvbata@gmail.com>
2016-06-27 13:57:44 -0800

    Implement a header and some styling container for the feeds

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,9 @@ var bfamRssApp = angular.module("bfamRssApp", ["ngSanitize", "ngRoute"]);
 
 bfamRssApp.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl: "about.html"
-    }).when("/landing-page", {
         templateUrl: "landingPage.html"
+    }).when("/about", {
+        templateUrl: "about.html"
     }).when("/edit-feeds", {
         templateUrl: "editFeeds.html"
     }).otherwise({
